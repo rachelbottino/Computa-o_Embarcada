b8d8af09385ad480ab167d5c9d284a68a4393006
rachelbottino <rachelpbm@gmail.com>
2017-09-18 18:05:48 -0300

    pio input

---
+++ Aulas/PIO Input/README.md
@@ -0,0 +1,41 @@
+---
+title: Computação Embarcada - PIO Outrput 
+author: Rafael Corsi - rafael.corsi@insper.edu.br
+date: Agosto 2017
+---
+
+Essa aula tem como objetivo trabalhar com entradas digitais do microcontrolador. 
+
+# Material de Aula
+
+- Slides PIO Input : [slides](https://github.com/Insper/Computacao-Embarcada/blob/master/1%20-%20Aulas/05%20-%20PIO%20output/07%20-%20%5BSlides%5D%20PIO%20OutPut.pdf)
+- Lista PIO Input : [lista](https://github.com/Insper/Computacao-Embarcada/blob/master/1%20-%20Aulas/05%20-%20PIO%20output/07%20-%20%5BPesquisa%5D%20PIO%20Output.pdf):
+
+# Projeto
+
+Utilize a placa : OLED1XPlained pro que possui mais 3 botões para, Utilize os mesmos
+passos anteriores para descobrir os pinos e o PIO referente a cada botão. Implemente as
+seguintes funcionalidades:
+
+1. Button 1 : Diminuir a frequência do piscar do led
+2. Button 3 : Aumentar a frequênciami
+
+Utilize o código exemplo que inicializa o botão e LED da placa, utilizando o estado do botão para definir o estado do LED.
+
+## Objetivos
+
+Estaremos trabalhando nessa etapa os seguintes itens dos objetivos de aprendizagem :
+
+1. Faz uso correto de define a fim de melhorar o entendimento/ manipulação do
+firmware
+1. Compreende como as informações extraídas do manual se traduzem para o código
+1. Correlaciona as diversas informações contidas em diferentes documentos.
+1. Faz uso de comentários
+1. Sabe usar corretamente as ferramentas de gravação e depuração
+1. Tem claro o fluxo de desenvolvimento
+
+## Avaliação
+
+
+
+ 
--- Aulas/PIO Input/src/src/main.c
+++ Aulas/PIO Input/src/src/main.c
@@ -1,6 +1,6 @@
 /**************************************************************************
-* Rafael Corsi   - Insper 
-* rafael.corsi@insper.edu.br        
+* Rafael Corsi   - Insper
+* rafael.corsi@insper.edu.br
 *
 * Computação Embarcada
 *
@@ -19,22 +19,26 @@
  * LEDs
  */
 #define LED_PIO_ID		ID_PIOC
-#define LED_PIO         PIOC
-#define LED_PIN			8
-#define LED_PIN_MASK	(1<<LED_PIN) 
+#define LED_PIO       PIOC
+#define LED_PIN			  8
+#define LED_PIN_MASK	(1<<LED_PIN)
 
 /**
  * Botão
- */ 
-#define BUT_PIO_ID		
-#define BUT_PIO         
-#define BUT_PIN			
-#define BUT_PIN_MASK	
+ */
+/**
+ * Botão
+ */
+#define BUT_PIO_ID      ID_PIOA
+#define BUT_PIO         PIOA
+#define BUT_PIN		    11
+#define BUT_PIN_MASK    (1 << BUT_PIN)
+#define BUT_DEBOUNCING_VALUE  79
 
 /************************************************************************/
 /* Prototipação                                                        */
 /************************************************************************/
-void ledConfig();
+//void ledConfig();
 
 /************************************************************************/
 /* Funções	                                                            */
@@ -43,9 +47,16 @@ void ledConfig();
 /**
  * @Brief Inicializa o pino do LED
  */
-void ledConfig(){
-	
+void ledConfig(int estado){
+	PMC->PMC_PCER0    = (1<<LED_PIO_ID);	    // Ativa clock do periférico no PMC
+	LED_PIO->PIO_PER  = LED_PIN_MASK;           // Ativa controle do pino no PIO    (PIO ENABLE register)
+	LED_PIO->PIO_OER  = LED_PIN_MASK;           // Ativa saída                      (Output ENABLE register)
+  if(!estado)                                 // Checa pela inicialização desejada
+    LED_PIO->PIO_CODR = LED_PIN_MASK;       // Coloca 0 na saída                (CLEAR Output Data register)
+  else
+    LED_PIO->PIO_SODR = LED_PIN_MASK;       // Coloca 1 na saída                (SET Output Data register)
 };
+
 
 /************************************************************************/
 /* Main                                                                 */
@@ -58,7 +69,7 @@ int main(void)
 	/************************************************************************/
 	sysclk_init();
 	WDT->WDT_MR = WDT_MR_WDDIS;
-	
+
 	/************************************************************************/
 	/* Inicializa perifericos                                               */
 	/************************************************************************/
@@ -66,13 +77,28 @@ int main(void)
 	ledConfig();
 
 	// Configura botao
-	
+	PMC->PMC_PCER0      = (1<<BUT_PIO_ID);     // Ativa clock do periférico no PMC
+	BUT_PIO->PIO_ODR	  = BUT_PIN_MASK;        // Desativa saída                   (Output DISABLE register)
+	BUT_PIO->PIO_PER	  = BUT_PIN_MASK;        // Ativa controle do pino no PIO    (PIO ENABLE register)
+	BUT_PIO->PIO_PUER	  = BUT_PIN_MASK;        // Ativa pull-up no PIO             (PullUp ENABLE register)
+	BUT_PIO->PIO_IFER	  = BUT_PIN_MASK;        // Ativa debouncing
+	BUT_PIO->PIO_IFSCER = BUT_PIN_MASK;        // Ativa clock periferico
+	BUT_PIO->PIO_SCDR	  = BUT_DEBOUNCING_VALUE;// Configura a frequencia do debouncing
+
 	/************************************************************************/
 	/* Super loop                                                           */
 	/************************************************************************/
 	while(1){
-	  
+		/**
+     * @Brief Verifica constantemente o status do botão
+     * 1 : não apertado
+     * 0 : apertado
+     */
+    if(BUT_PIO->PIO_PDSR & (BUT_PIN_MASK)){
+			LED_PIO->PIO_CODR = LED_PIN_MASK;
+    }
+		else{
+			LED_PIO->PIO_SODR = LED_PIN_MASK;
+    }
 	};
 }
-
-
